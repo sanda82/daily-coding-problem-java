a278438a1fda71bc2b3bb067247ae4ec4f5d8450
Chinthaka Sandaruwan <sanda.mdtric@gmail.com>
2023-01-29 21:35:13 +0100

    Problem #1270

---
+++ src/com/problem/Problem01270.java
@@ -0,0 +1,18 @@
+package com.problem;
+
+public class Problem01270 {
+
+    public static String W = "ab";
+    public static String S = "abxaba";
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int[] getIndices(String W, String S) {
+        int[] indices =  new int[];
+
+
+        return indices;
+    }
+}
